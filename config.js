f287615f341914eaa844f00bc44e6a2c703692cc
Ulises Montenegro <103386507+ulisesmontenegro2005@users.noreply.github.com>
2023-04-27 21:15:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 export default {
-    PORT: process.env.PORT || 8080,
+    PORT: process.env.PORT,
     MONGO: process.env.MONGO,
     EMAIL: process.env.EMAIL_ACCOUNT,
     EMAIL_PASS: process.env.EMAIL_PASSWORD
